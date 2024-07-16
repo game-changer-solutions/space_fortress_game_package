26c78d13616f43368aff07ca8decea7f3ac8546a
M074MED <mhmedmhmed1234951@gmail.com>
2024-07-16 21:20:36 +0300

    fix some integration issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,17 @@
 import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:space_fortress_game_package/init.dart';
 import 'package:space_fortress_game_package/language_constants.dart';
 import 'package:space_fortress_game_package/screens/main_menu.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:space_fortress_game_package/theme/app_theme.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
   runApp(const MyApp());
 }
 
@@ -36,24 +39,9 @@ class _MyAppState extends State<MyApp> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    Flame.device.fullScreen();
-    SystemChrome.setPreferredOrientations(
-        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
-  }
-
-  @override
   void didChangeDependencies() {
     getLocale().then((locale) => {setLocale(locale)});
     super.didChangeDependencies();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   }
 
   @override
@@ -62,90 +50,11 @@ class _MyAppState extends State<MyApp> {
       title: "Space Fortress",
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
-      darkTheme: ThemeData(
-        // brightness: Brightness.dark,
-        fontFamily: "BungeeInline",
-        scaffoldBackgroundColor: Colors.black,
-        useMaterial3: false,
-        colorScheme: integrationInitialized && primaryColor != null
-            ? ColorScheme.fromSeed(seedColor: primaryColor!)
-            : null,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: integrationInitialized && primaryColor != null
-                ? primaryColor!
-                : null,
-          ),
-        ),
-        switchTheme: SwitchThemeData(
-          thumbColor:
-              WidgetStateProperty.resolveWith((Set<WidgetState> states) {
-            if (states.contains(WidgetState.selected)) {
-              return integrationInitialized && primaryColor != null
-                  ? primaryColor!
-                  : null;
-            }
-            return null;
-          }),
-          trackColor:
-              WidgetStateProperty.resolveWith((Set<WidgetState> states) {
-            if (states.contains(WidgetState.selected)) {
-              return integrationInitialized && primaryColor != null
-                  ? primaryColor!.withOpacity(0.7)
-                  : null;
-            }
-            return integrationInitialized && primaryColor != null
-                ? primaryColor!.withOpacity(0.3)
-                : null;
-          }),
-        ),
-      ),
       home: const MainMenu(),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      locale: integrationInitialized && localName != null
-          ? Locale(localName!)
-          : _locale,
-      theme: ThemeData(
-        fontFamily: "BungeeInline",
-        scaffoldBackgroundColor: Colors.black,
-        useMaterial3: false,
-        colorScheme: integrationInitialized && primaryColor != null
-            ? ColorScheme.fromSeed(seedColor: primaryColor!)
-            : null,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: integrationInitialized && primaryColor != null
-                ? primaryColor!
-                : null,
-          ),
-        ),
-        switchTheme: SwitchThemeData(
-          thumbColor:
-              WidgetStateProperty.resolveWith((Set<WidgetState> states) {
-            if (states.contains(WidgetState.selected)) {
-              return integrationInitialized && primaryColor != null
-                  ? primaryColor!
-                  : null;
-            }
-            return null;
-          }),
-          trackColor:
-              WidgetStateProperty.resolveWith((Set<WidgetState> states) {
-            if (states.contains(WidgetState.selected)) {
-              return integrationInitialized && primaryColor != null
-                  ? primaryColor!.withOpacity(0.7)
-                  : null;
-            }
-            return integrationInitialized && primaryColor != null
-                ? primaryColor!.withOpacity(0.3)
-                : null;
-          }),
-        ),
-      ),
-      // themeMode: integrationInitialized && themeMode != null
-      //     ? themeMode
-      //     : ThemeMode.light,
+      locale: _locale,
+      theme: AppTheme.apptheme,
     );
   }
 }
--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -1,11 +1,14 @@
 import 'dart:math';
 
+import 'package:flame/flame.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:space_fortress_game_package/init.dart';
 import 'package:space_fortress_game_package/language_constants.dart';
 import 'package:space_fortress_game_package/main.dart';
 import 'package:space_fortress_game_package/screens/game_play.dart';
 import 'package:space_fortress_game_package/screens/settings_menu.dart';
+import 'package:space_fortress_game_package/theme/app_theme.dart';
 import 'package:space_fortress_game_package/widgets/snack_bar.dart';
 
 String getRandomChar() {
@@ -38,121 +41,154 @@ class _MainMenuState extends State<MainMenu> {
   void initState() {
     super.initState();
     InitApp.initializeApp(context);
+    Flame.device.fullScreen();
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight]);
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
-      floatingActionButton: !integrationInitialized
-          ? ElevatedButton(
-              onPressed: () async {
-                Locale locale = await setLocale(
-                    translation(context).localeName == "en" ? "ar" : "en");
-                MyApp.setLocale(context, locale);
-              },
-              child: Text(translation(context).changeLanguage,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    fontFamily: "Roboto",
-                  )),
-            )
-          : Container(),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(vertical: 25.0),
-                child: Text(
-                  "Space Fortress",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 50.0,
-                    shadows: [
-                      Shadow(
-                        blurRadius: 40.0,
-                        color: Colors.white,
-                        offset: Offset(0, 0),
+    return PopScope(
+      onPopInvoked: (didPop) {
+        if (didPop) {
+          SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+          SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+        }
+      },
+      child: Theme(
+        data: AppTheme.apptheme,
+        child: Scaffold(
+          floatingActionButtonLocation: FloatingActionButtonLocation.endTop,
+          floatingActionButton: !integrationInitialized
+              ? ElevatedButton(
+                  onPressed: () async {
+                    Locale locale = await setLocale(
+                        translation(context).localeName == "en" ? "ar" : "en");
+                    MyApp.setLocale(context, locale);
+                  },
+                  child: Text(translation(context).changeLanguage,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        fontFamily: "Roboto",
+                      )),
+                )
+              : Container(),
+          body: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 25.0),
+                    child: Text(
+                      "Space Fortress",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 50.0,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 40.0,
+                            color: Colors.white,
+                            offset: Offset(0, 0),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              ),
-              if (!integrationInitialized)
-                SizedBox(
-                  width: MediaQuery.of(context).size.width / 3,
-                  height: 40,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      iconColor: Colors.white,
-                      labelText: translation(context).username,
-                      border: const OutlineInputBorder(),
-                      enabledBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white)),
-                      labelStyle: const TextStyle(color: Colors.white),
-                    ),
-                    controller: usernameInput,
-                    style: const TextStyle(
-                      fontFamily: "",
-                      color: Colors.white,
-                      fontSize: 16,
                     ),
                   ),
-                ),
-              const SizedBox(
-                height: 15,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width / 3,
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (usernameInput.text == "" && !integrationInitialized) {
-                      showSnackBar(
-                          context, translation(context).usernameRequiredField);
-                    } else {
-                      Navigator.of(context).pushReplacement(
-                        MaterialPageRoute(
-                          builder: (context) => GamePlay(),
+                  Text(
+                    "${translation(context).codeMines}\n${foeMinesCode.join(",")}",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  if (!integrationInitialized)
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      height: 40,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          iconColor: Colors.white,
+                          labelText: translation(context).username,
+                          border: const OutlineInputBorder(),
+                          enabledBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white)),
+                          labelStyle: const TextStyle(color: Colors.white),
                         ),
-                      );
-                    }
-                  },
-                  child: Text(
-                    translation(context).play,
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              SizedBox(
-                width: MediaQuery.of(context).size.width / 3,
-                child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => const SettingsMenu(),
+                        controller: usernameInput,
+                        style: const TextStyle(
+                          fontFamily: "",
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
                       ),
-                    );
-                  },
-                  child: Text(
-                    translation(context).settings,
+                    ),
+                  const SizedBox(
+                    height: 15,
                   ),
-                ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 3,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (usernameInput.text == "" &&
+                            !integrationInitialized) {
+                          showSnackBar(context,
+                              translation(context).usernameRequiredField);
+                        } else {
+                          Navigator.of(context).pushReplacement(
+                            MaterialPageRoute(
+                              builder: (context) => GamePlay(),
+                            ),
+                          );
+                        }
+                      },
+                      child: Text(
+                        translation(context).play,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width / 3,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const SettingsMenu(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        translation(context).settings,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  if (integrationInitialized)
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width / 3,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          translation(context).exit,
+                        ),
+                      ),
+                    ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
               ),
-              const SizedBox(
-                height: 10,
-              ),
-              Text(
-                "${translation(context).codeMines}\n${foeMinesCode.join(",")}",
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 18, color: Colors.white),
-              ),
-            ],
+            ),
           ),
         ),
       ),
